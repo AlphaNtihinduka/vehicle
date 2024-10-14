374c0e7657c64055deb4e7698b00a5fd7ae08d04
AlphaNtihinduka <ntihindukaalpha@yahoo.com>
2024-10-14 07:22:21 +0300

    Implement vehicle abstract

---
+++ src/Vehicle.java
@@ -0,0 +1,15 @@
+public abstract class Vehicle {
+    String type;
+    int numberOfWheels;
+
+    public Vehicle(String type, int numberOfWheels) {
+        this.type = type;
+        this.numberOfWheels = numberOfWheels;
+    }
+
+    public void showVehicleDetails() {
+        System.out.println("Car type: "+ type+ ", Number of wheels: "+numberOfWheels);
+    }
+
+    abstract void doVehicleSound();
+}
