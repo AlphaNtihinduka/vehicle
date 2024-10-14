ab5e0b6ae2cda74b72cad5c50809e90e294e43bb
AlphaNtihinduka <ntihindukaalpha@yahoo.com>
2024-10-14 07:54:28 +0300

    Implement Boat class and methods

---
+++ src/Boat.java
@@ -0,0 +1,20 @@
+public class Boat extends Vehicle{
+    double maxKnotsSpeed;
+    int boatKilosWeight;
+
+    public Boat(double maxKnotsSpeed, int boatKilosWeight) {
+        super("Boat", 0);
+        this.maxKnotsSpeed = maxKnotsSpeed;
+        this.boatKilosWeight = boatKilosWeight;
+    }
+
+    @Override
+    public void doVehicleSound() {
+        System.out.println("Boat sound: Bubble loud acoustic sound");
+    }
+
+    public String getBoatWeightAndSpeed() {
+        return "Max knot speed: "+maxKnotsSpeed+"Boat weight: "+boatKilosWeight;
+    }
+}
+
