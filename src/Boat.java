ba35e900d3cc747f488d976b560a3fb267071d7a
AlphaNtihinduka <ntihindukaalpha@yahoo.com>
2024-10-14 08:02:09 +0300

    Add comments

--- src/Boat.java
+++ src/Boat.java
@@ -2,6 +2,7 @@ public class Boat extends Vehicle{
     double maxKnotsSpeed;
     int boatKilosWeight;
 
+    // Create a boat constructor
     public Boat(double maxKnotsSpeed, int boatKilosWeight) {
         super("Boat", 0);
         this.maxKnotsSpeed = maxKnotsSpeed;
@@ -9,10 +10,13 @@ public class Boat extends Vehicle{
     }
 
     @Override
+
+    // Override the vehicle sound
     public void doVehicleSound() {
         System.out.println("Boat sound: Bubble loud acoustic sound");
     }
 
+    // print the boat speed and weight details
     public String getBoatWeightAndSpeed() {
         return "Max knot speed: "+maxKnotsSpeed+", Boat weight: "+boatKilosWeight;
     }
